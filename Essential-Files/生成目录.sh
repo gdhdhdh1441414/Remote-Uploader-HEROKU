d8aa7c876a2720b8e28c35e3f65a4696bc005107
10362227 <10362227@163.com>
2022-03-29 16:16:39 +0800

    Update 生成目录.sh

--- Essential-Files/生成目录.sh
+++ Essential-Files/生成目录.sh
@@ -1,6 +1,7 @@
-echo "<meta http-equiv='Content-Type' content='text/html; charset=utf-8' /><head>" > /usr/local/share/jupyter/voila/templates/base/static/files/files.html 
+echo "<meta http-equiv='Content-Type'content='text/html;charset=utf-8'/><head>" > /usr/local/share/jupyter/voila/templates/base/static/files/files.html 
 echo "<html><table>" >> /usr/local/share/jupyter/voila/templates/base/static/files/files.html 
 find /usr/local/share/jupyter/voila/templates/base/static/files/. -maxdepth 2 -type f -printf '<li><a href=%f>%f</td></tr>\n' >>  /usr/local/share/jupyter/voila/templates/base/static/files/files.html
 echo "</table></html>" >>  /usr/local/share/jupyter/voila/templates/base/static/files/files.html
 sed  -i "s/ /%20/g" /usr/local/share/jupyter/voila/templates/base/static/files/files.html
 sed  -i "s/a%20href/a href/g" /usr/local/share/jupyter/voila/templates/base/static/files/files.html 
+sed  -i "s/meta%20http/meta http/g" /usr/local/share/jupyter/voila/templates/base/static/files/files.html 
