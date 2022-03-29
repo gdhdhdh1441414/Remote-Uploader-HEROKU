16c5a3db5ab09312659274f623d983554c4e8b3e
10362227 <10362227@163.com>
2022-03-29 15:54:33 +0800

    Update 生成目录.sh

--- Essential-Files/生成目录.sh
+++ Essential-Files/生成目录.sh
@@ -1,3 +1,4 @@
+echo "<meta http-equiv='Content-Type' content='text/html; charset=utf-8' /><head>" > /usr/local/share/jupyter/voila/templates/base/static/files/files.html 
 echo "<html><table>" > /usr/local/share/jupyter/voila/templates/base/static/files/files.html 
 find /usr/local/share/jupyter/voila/templates/base/static/files/. -maxdepth 2 -type f -printf '<li><a href=%f>%f</td></tr>\n' >>  /usr/local/share/jupyter/voila/templates/base/static/files/files.html
 echo "</table></html>" >>  /usr/local/share/jupyter/voila/templates/base/static/files/files.html
